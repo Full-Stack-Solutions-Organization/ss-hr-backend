27218a6ff46b3d21f5533a110ba68033d7d5b277
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-10-28 16:21:12 +0530

    refactor : app.ts updated for testing

--- src/app.ts
+++ src/app.ts
@@ -27,35 +27,44 @@ const allowedOrigins = [appConfig.frontendUrl];
 
 app.use(cors({
   origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    // Testing only
+    // TODO remove this after testing
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(null, false);
+
+    // if (!origin || allowedOrigins.includes(origin)) {
+    //   callback(null, true);
+    // } else {
+    //   callback(new Error("Not allowed by CORS"));
+    // }
+  },
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 }));
 
 app.use(compression());
 app.use(helmet());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-app.use('/api/auth',authRoutes);
+app.use('/api/auth', authRoutes);
 
-app.use("/api/admin/settings",adminSettingsRoutes);
-app.use('/api/admin/jobs',adminJobRoutes);
-app.use('/api/admin/chat',adminChatRoutes);
-app.use('/api/admin/users',adminUsersRoutes);
-app.use("/api/admin/testimonials",adminTestimonialRoutes);
+app.use("/api/admin/settings", adminSettingsRoutes);
+app.use('/api/admin/jobs', adminJobRoutes);
+app.use('/api/admin/chat', adminChatRoutes);
+app.use('/api/admin/users', adminUsersRoutes);
+app.use("/api/admin/testimonials", adminTestimonialRoutes);
 app.use('/api/admin/packages', adminPackageRoutes);
 app.use('/api/admin/payments', adminPaymentRoutes);
-app.use('/api/message',messageRoutes);
-app.use('/api/user',userRoutes);
+app.use('/api/message', messageRoutes);
+app.use('/api/user', userRoutes);
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({

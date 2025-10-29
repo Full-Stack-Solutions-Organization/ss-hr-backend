67eaebad4f0cdd2daf5a6e68e5f6f85b1809d1bb
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-10-29 16:43:17 +0530

    refactor : Socket server test 5

--- src/app.ts
+++ src/app.ts
@@ -42,10 +42,12 @@ app.use(
   cors({
     origin: [
       "https://www.sshrconsultancy.com", // your frontend domain
-      "https://sshrconsultancy.com"      // optional (without www)
+      "https://sshrconsultancy.com",  // optional (without www)
+      "http://localhost:5000"      // optional (without www)
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true, // if you're using cookies or Authorization headers
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
   })
 );
 

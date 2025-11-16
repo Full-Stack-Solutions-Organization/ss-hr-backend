f1db9871937db8f177843cae9f9009200115edc5
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-11-16 20:43:19 +0530

    refactor: testimonials fetching bug fixed

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,7 @@ import adminApplicationRouter from './presentation/routes/adminApplicationRouter
 const app = express();
 
 if (appConfig.nodeEnv !== 'development') {
+  console.log("here");
   app.use((req, res, next) => {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(`https://${req.get('Host')}${req.url}`);
